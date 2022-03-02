2ad29192fb82b1a4a7b511bb72e4bfa21ffb1ac5
Luis López <lopezsolerluis@gmail.com>
2022-03-02 16:31:38 -0300

    mínimos cambios

--- src/lopezsolerluis/fits.cljs
+++ src/lopezsolerluis/fits.cljs
@@ -3,11 +3,9 @@
 (defn leer-cabecera [array]
   (let [primera-linea (.slice array 0 30)
         segunda-linea (.slice array 80 110)]
-    (if false ;;(not= primera-linea "SIMPLE  =                    T")
+    (if (not= primera-linea "SIMPLE  =                    T")
       :fits-no-simple
       (do
-        ;(js/console.log (apply str (map char primera-linea)))
-        ;(js/console.log (apply str (map char segunda-linea)))
         (apply str (map char primera-linea))))))
 
 (defn read-fits-file [file callback]
@@ -16,9 +14,9 @@
     (let [js-file-reader (js/FileReader.)]
       (set! (.-onload js-file-reader)
         (fn [evt]
-          (let [resultado (-> evt .-target .-result)
-                array (js/Uint8Array. resultado)
-                res (leer-cabecera array)]
+          (let [contenido (-> evt .-target .-result)
+                array (js/Uint8Array. contenido)
+                cabecera (leer-cabecera array)]
             (callback res))))
       (.readAsArrayBuffer js-file-reader file))))
 
