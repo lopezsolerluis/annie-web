be3a829aa8c8ec623461bceb54d6208a23d045b6
Luis López <lopezsolerluis@gmail.com>
2022-03-03 10:26:30 -0300

    array->string

--- src/lopezsolerluis/annie_web.cljs
+++ src/lopezsolerluis/annie_web.cljs
@@ -24,21 +24,19 @@
         (gdom/setTextContent el (app-tr lang (keyword "menu" nombre)))))))
 ;; end of translation functions
 
-(defn mostrar-resultado [resultado]
-  (js/console.log "Mostrar-resultado: " resultado))
+(defn procesar-archivo [result]
+  (js/console.log "Fits: " result))
+  ; (cond
+  ;     (= :extensión-no-fits resultado) (js/alert (app-tr @lang :extensión-no-fits))
+  ;     (= :fits-no-simple resultado)    (js/alert (app-tr @lang :fits-no-simple))))
 
 (defn input-file []
   [:input {:type "file" :id "fits" :name "imagenFits" :accept "image/fits" ;; este atributo no funciona...
            :on-change (fn [this]
                         (if (not (= "" (-> this .-target .-value)))
-                          (let [^js/File file (-> this .-target .-files (aget 0))
-                                resultado (fits/read-file file mostrar-resultado)]
-                            (js/console.log "Final: " resultado)
-                             (cond
-                               (= :extensión-no-fits resultado) (js/alert (app-tr @lang :extensión-no-fits))
-                               (= :fits-no-simple resultado)    (js/alert (app-tr @lang :fits-no-simple))))
-                        (set! (-> this .-target .-value) "")))}])
-
+                          (let [^js/File file (-> this .-target .-files (aget 0))]
+                            (fits/read-fits-file file procesar-archivo)))
+                          (set! (-> this .-target .-value) ""))}])
 
 (defonce is-initialized?
   (do
--- src/lopezsolerluis/fits.cljs
+++ src/lopezsolerluis/fits.cljs
@@ -1,24 +1,38 @@
 (ns lopezsolerluis.fits)
 
-(defn leer-cabecera [array]
-  (let [primera-linea (.slice array 0 30)
-        segunda-linea (.slice array 80 110)]
+(defn array->string
+  ([array]
+    (->> array
+      (map char)
+      (apply str)))
+  ([array start end]
+    (-> array
+      (.slice start end)
+      (array->string))))
+  ;;(apply str (map char array)))
+
+(defn leer-cabecera [uint8array]
+  (let [primera-linea (array->string uint8array 0 30)]
+    (js/console.log "Primera línea: " primera-linea)
     (if (not= primera-linea "SIMPLE  =                    T")
-      :fits-no-simple
-      (do
-        (apply str (map char primera-linea))))))
+        :fits-no-simple
+        :fits-simple
+    )))
+  ;
+  ;       segunda-linea (.slice array 80 110)]
+  ;
+  ;     (do
+  ;       (apply str (map char primera-linea))))))
 
 (defn read-fits-file [file callback]
-  (if (not= (.-type file) "image/fits")
-    (callback :extensión-no-fits)
-    (let [js-file-reader (js/FileReader.)]
-      (set! (.-onload js-file-reader)
-        (fn [evt]
-          (let [contenido (-> evt .-target .-result)
-                array (js/Uint8Array. contenido)
-                cabecera (leer-cabecera array)]
-            (callback cabecera))))
-      (.readAsArrayBuffer js-file-reader file))))
+  (let [js-file-reader (js/FileReader.)]
+    (set! (.-onload js-file-reader)
+      (fn [evt]
+        (let [contenido (-> evt .-target .-result)
+              uint8array (js/Uint8Array. contenido)
+              cabecera (leer-cabecera uint8array)]
+          (callback cabecera))))
+    (.readAsArrayBuffer js-file-reader file)))
 
 (defn read-file [file callback]
   (let [js-file-reader (js/FileReader.)]
