9242502860d5ad7bf819d67e406129c08e4e74af
Luis G. López <lopezsolerluis@gmail.com>
2022-03-16 16:34:59 -0300

    Agregado de archivos auxiliares

---
+++ prueba-mapa.cljs
@@ -0,0 +1,7 @@
+(def mapa {:pestaña-1 {:perfil-1 {:etiquetas {:etiqueta-1 {:x 1 :y 1}} :etiqueta-2 {:x 2 :y 2}}}
+                      {:perfil-2 {:etiquetas {:etiqueta-1 {:x 1 :y 1}} :etiqueta-2 {:x 2 :y 2}}}
+                      {:perfil-3 {:etiquetas {:etiqueta-1 {:x 1 :y 1}} :etiqueta-2 {:x 2 :y 2}}}
+           :pestaña-2 {:perfil-1 {:etiquetas {:etiqueta-1 {:x 1 :y 1}} :etiqueta-2 {:x 2 :y 2}}}
+                      {:perfil-2 {:etiquetas {:etiqueta-1 {:x 1 :y 1}} :etiqueta-2 {:x 2 :y 2}}}
+                      {:perfil-3 {:etiquetas {:etiqueta-1 {:x 1 :y 1}} :etiqueta-2 {:x 2 :y 2}}}
+          })
